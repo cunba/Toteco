78da45d49efa03c346a5f961fd6cb59cef932337
cunba <ire.cunba@gmail.com>
2024-06-21 17:50:37 +0200

    minor changes

--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -100,7 +100,7 @@ export const ProfileView: FunctionalView<ProfileViewModel> = ({ vm }) => {
                         </View>
                         {isUserLogged ?
                             null :
-                            <TouchableOpacity style={[formStyles.button, profileStyles.followButton, { backgroundColor: COLORS.touchable }]} >
+                            <TouchableOpacity style={[formStyles.button, profileStyles.followButton, { backgroundColor: isFollowing ? COLORS.background_second : COLORS.touchable }]} >
                                 <Text style={[commonStyles.text, { color: COLORS.text_touchable, fontSize: SIZES.text }]}>{isFollowing ? i18n.t('profile.button.unfollow') : i18n.t('profile.button.follow')}</Text>
                             </TouchableOpacity>
                         }
